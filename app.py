ba3022be81f67c26aa28818d06a0c0028a70cf47
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2021-12-26 12:17:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ class VideoTransformer(VideoTransformerBase):
             cv2.putText(img,label, (50,60), cv2.FONT_HERSHEY_SCRIPT_COMPLEX,2, (120,10,200),3)    
         return img
 def main():
+    my_model=load_model('model.h5')
     webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 if __name__ == "__main__":
     main()
